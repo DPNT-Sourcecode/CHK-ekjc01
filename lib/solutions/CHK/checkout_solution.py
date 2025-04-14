baf898e282bc8d8bc2dd8399d7f9e60987617692
sbx_user1051 <sbx_user1051@169.254.6.143>
2025-04-14 15:25:12 +0000

    Mon Apr 14 15:25:12 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,13 +36,14 @@ class CheckoutSolution:
         total += counts.get("D", 0) * 15
         total += counts.get("E", 0) * 40
         total += counts.get("G", 0) * 40
-        total += counts.get("E", 0) * 40
-        total += counts.get("E", 0) * 40
-        total += counts.get("E", 0) * 40
-        total += counts.get("E", 0) * 40
-        total += counts.get("E", 0) * 40
+        total += counts.get("I", 0) * 40
+        total += counts.get("J", 0) * 40
+        total += counts.get("K", 0) * 40
+        total += counts.get("L", 0) * 40
+        total += counts.get("M", 0) * 40
         total += counts.get("E", 0) * 40
         total += counts.get("E", 0) * 40
 
     
         return total
+
