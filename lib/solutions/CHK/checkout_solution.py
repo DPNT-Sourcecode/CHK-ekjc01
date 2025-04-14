e86c83ca34b6c552aa95b41115a011aababaf037
sbx_user1051 <sbx_user1051@169.254.6.143>
2025-04-14 15:10:13 +0000

    Mon Apr 14 15:10:13 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,7 +11,7 @@ class CheckoutSolution:
         
        # multi-item special offers
 
-       # E's Offer: for every 2 Es, get 1 B for free
+       # E's Offer: for every 2 Es, get 1 B free
         free_b = counts.get("E", 0) // 2
         counts["B"] = max(0, counts.get("B", 0) - free_b)
 
@@ -20,13 +20,12 @@ class CheckoutSolution:
         payable_f = (f // 3) * 2 + (f % 3)
         total += payable_f * 10
 
-        payable_f = (f // 3) * 2 + (f % 3)
-        total += payable_f * 10
-
+       # A's Offer 
         a = counts.get("A", 0)
         remainder_a = a % 5
         total += (a // 5) * 200
         total += (remainder_a // 3) * 130 + (remainder_a % 3) * 50
+
 
         b = counts.get("B", 0)
         total += (b // 2) * 45 + (b % 2) * 30
@@ -39,3 +38,4 @@ class CheckoutSolution:
 
     
         return total
+
