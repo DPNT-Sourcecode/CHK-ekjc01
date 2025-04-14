485e9f652eff1148d3fca7a10502aa49447c51b5
sbx_user1051 <sbx_user1051@169.254.6.143>
2025-04-14 14:29:59 +0000

    Mon Apr 14 14:29:59 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,8 @@
 from collections import Counter
 
 class CheckoutSolution:
-    # skus = unicode string
     def checkout(self, skus):
+        
         if not all(c in "ABCD" for c in skus):
             return -1
         
@@ -11,7 +11,7 @@ class CheckoutSolution:
         total = 0
 
         total += (counts["A"] // 3) * 130 + (counts["A"] % 3) * 50
-        total += (counts["B"] // 2) * 45 + (counts["B % 3) * 50
+        total += (counts["B"] // 2) * 45 + (counts["B"] % 2) * 30
         total += counts["C"] * 20
         total += counts["D"] * 15
 
